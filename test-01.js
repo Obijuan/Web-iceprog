f2022841051263b4b5d27fa571aeaa9d1b49bc66
Obijuan <obijuan.cube@gmail.com>
2021-04-30 04:42:09 +0200

    first controlTransferOut

--- test-01.js
+++ test-01.js
@@ -1,4 +1,15 @@
-console.log("Holiii");
+// FTDI USB identifiers
+const usbVendor = 0x0403;
+const usbProduct = 0x6010;
+const BITMODE_MPSSE  = 0x02;
+const INTERFACE_A   = 1;
+
+
+//-- Important information
+// ftdi->interface = 0;
+// ftdi->index     = INTERFACE_A;
+// ftdi->in_ep     = 0x02; //-- Endpoint!
+// ftdi->out_ep    = 0x81; //-- Endpoint!
 
 const btn_usb = document.getElementById('btn_usb');
 const display = document.getElementById('display');
@@ -11,6 +22,16 @@ if ('usb' in navigator == false) {
 
 let device;
 
+const LIBUSB_REQUEST_TYPE_VENDOR = (0x02 << 5);
+const LIBUSB_RECIPIENT_DEVICE = 0X00;
+const LIBUSB_ENDPOINT_OUT =  0x00;
+const LIBUSB_ENDPOINT_IN = 0x80;
+const FTDI_DEVICE_OUT_REQTYPE = LIBUSB_REQUEST_TYPE_VENDOR |
+                                LIBUSB_RECIPIENT_DEVICE |
+                                LIBUSB_ENDPOINT_OUT; 
+const SIO_RESET_REQUEST = 0;  //-- Reset the port
+const SIO_RESET_SIO = 0;
+
 btn_usb.onclick = () => {
     console.log("Connect to USB");
 
@@ -19,10 +40,26 @@ btn_usb.onclick = () => {
        device = sel_device;
        display.innerHTML = device.productName + " " + device.manufacturerName;
        return device.open();
-     })
-    .then ( () => {
-        console.log("El programa sigue por aquí...");
+       })
+    .then ( () => device.controlTransferOut({
+          requestType: 'vendor',
+          recipient: 'device',
+          request: SIO_RESET_REQUEST,
+          value: SIO_RESET_SIO,
+          index: INTERFACE_A
+        }))
+    .then ( status => {
+        console.log("Control transfer out...");
+        console.log("Promise: ");
+        console.log(status);
+        console.log("Ya...");
     })
+//  if (libusb_control_transfer(ftdi->usb_dev, FTDI_DEVICE_OUT_REQTYPE,
+//      SIO_RESET_REQUEST, SIO_RESET_SIO,
+//      ftdi->index, NULL, 0, ftdi->usb_write_timeout) < 0)
+//    ftdi_error_return(-1,"FTDI reset failed");
+
+        
     .catch(error => { 
         console.error(error); 
     });
