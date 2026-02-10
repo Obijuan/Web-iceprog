9f939e9115602e3f6972f077afa49717ce7813be
obijuan <obijuan.cube@gmail.com>
2026-02-10 10:00:40 +0100

    test-03: update

--- wiki/web-iceprog/test-03.js
+++ wiki/web-iceprog/test-03.js
@@ -521,11 +521,11 @@ async function flash_write_enable(device, verbose)
   //await flash_chip_select();
 
 
-  
+
   //await mpsse_xfer_spi(buff);
-  await mpsse_send_byte(MC_DATA_IN | MC_DATA_OUT | MC_DATA_OCN);
-  await mpsse_send_byte(buff.byteLength - 1);
-  await mpsse_send_byte((buff.byteLength - 1) >> 8);
+  await mpsse_send_byte(0x31);
+  await mpsse_send_byte(0);
+  await mpsse_send_byte(0);
 
   let rc = await ftdi_write_data(device, buff);
   for (let i = 0; i < buff.byteLength; i++)
