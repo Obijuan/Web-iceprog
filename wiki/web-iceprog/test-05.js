1757f86befb65c797295cb12bd1798b73b9a108f
obijuan <obijuan.cube@gmail.com>
2026-02-17 11:56:05 +0100

    test-05: clean up code

--- wiki/web-iceprog/test-05.js
+++ wiki/web-iceprog/test-05.js
@@ -1,31 +1,11 @@
 import * as ftdi from './ftdi.js';
 
 const btn_usb = document.getElementById('btn_usb');
-
-
-// 🚧 DEBUG 🚧  
 const display = document.getElementById('display');
 const btn_list = document.getElementById('btn_list');
 const btn_close = document.getElementById('btn_close');
 const bitstream = document.getElementById('bitstream');
 
-const MC_DATA_IN  =  0x20 // When set read data (Data IN)
-const MC_DATA_OUT =  0x10 // When set write data (Data OUT)
-const MC_DATA_OCN = 0x01  // When set update data on negative clock edge
-
-// ---------------------------------------------------------
-// FLASH definitions
-// ---------------------------------------------------------
-
-// Flash command definitions
-// This command list is based on the Winbond W25Q128JV Datasheet
-const FC_PD = 0xB9; // Power-down
-
-//-- Important information
-// ftdi->interface = 0;
-// ftdi->index     = INTERFACE_A;
-const IN_EP = 0x02; //-- Endpoint for transfering data from host to device
-const OUT_EP = 0x01; //-- Endpoint!  0x81
 
 btn_list.onclick = async () => {
     let devices = await navigator.usb.getDevices();
@@ -48,118 +28,6 @@ navigator.usb.addEventListener('disconnect', event => {
     display.innerHTML = "";
 })
 
-
-//----- FTDI: Write_data
-//-- Escribir un buffer en el FTDI
-//-- Tamaño máximo buffer: 4096
-async function ftdi_write_data(device, buff)
-{
-  let result = await device.transferOut(IN_EP, buff); 
-
-  //console.log("FTDI_WRITE: Buffer written: " + result.status);
-  //console.log("  -> Written: " + result.bytesWritten + " byte(s)");
-
-  return result.bytesWritten;
-}
-
-//-- MPSSE: Send one byte
-async function mpsse_send_byte(b) {
-
-  let data = new Uint8Array(1);
-  data[0] = b;
-  let result = await device.transferOut(IN_EP, data); 
-
-  //console.log("MPSSE: Send_byte: " + result.status);
-  //console.log("  -> Written: " + result.bytesWritten + ", Value: 0x" + b.toString(16));
-}
-
-//-------- MPSSE: mpsse_recv_byte()
-async function mpsse_recv_byte(device) {
-
-  //console.log("queue length: " + queue.length);
-
-  //-- Byte to read
-  let data;
-
-  //-- There at least 1 byte in the buffer. There is no need to
-  //-- access the USB
-  if (queue.length >= 1) {
-
-    //-- Read the first element in the buffer
-    data = queue.shift();
-
-    //console.log("MPSSE: recv_byte. Byte in buffer: " + data.toString(16));
-    return data;
-  }
-
-  //-- Buffer is empty. Read data from the USB
-  let result = await device.transferIn(OUT_EP, 4096);
-
-  // console.log("TransferIn: " + result.status +
-  // " -> Bytes: " + result.data.byteLength);
-
-  let cad = "";
-
-  //-- The first two bytes received are the modem status bytes
-  //-- Insert the data in the queue
-  for (let i = 2; i < result.data.byteLength; i = i + 1) {
-    queue.push(result.data.getUint8(i));
-    cad = cad + "0x" + result.data.getUint8(i).toString(16) + " ";
-  }
-
-  //console.log("QUEUE: [ " + cad + "]");
-
-  //-- Read the first element in the queue
-  if (queue.length > 0) {
-    data = queue.shift();
-    //console.log("MPSEE: recv_byte. Read: " + data.toString(16) + 
-    //            "Buffer size: " + queue.length);
-    return data;
-  }
-
-  //console.log("MPSSE: recv_byte. NO DATA READ! (EMPTY)");
-
-  //return -1;
-}
-
-
-//------ MPSSE: xfer_spi()
-async function mpsse_xfer_spi(buff)
-{
-  //console.log("MPSSE: xfer_spi. START!---------")
-   if (buff.byteLength < 1)
-     return;
-
-  /* Input and output, update data on negative edge read on positive. */
-  await mpsse_send_byte(MC_DATA_IN | MC_DATA_OUT | MC_DATA_OCN);
-  await mpsse_send_byte(buff.byteLength - 1);
-  await mpsse_send_byte((buff.byteLength - 1) >> 8);
-
-  let rc = await ftdi_write_data(device, buff);
-  //-- Todo! Check the correct number of bytes has been written....
-
-  //console.log("Rc: " + rc + ", Buff lenth: " + buff.byteLength);
-
-  for (let i = 0; i < buff.byteLength; i++)
-    buff[i] = await mpsse_recv_byte(device);
-
-  //console.log("MPSSE: xfer_spi. Written: " + rc + " byte(s)!");
-  //console.log("MPSSE: xfer_spio. STOP!----------------")
-}
-
-
-// ---------------------------------------------------------
-// FLASH function implementations
-// ---------------------------------------------------------
-
-//async function flash_power_down()
-//{
-//  let buff = new Uint8Array(1);
-//  buff[0] = FC_PD;
-//  await ftdi.FLASH_cs_assert(device);
-//  await mpsse_xfer_spi(buff);
-//  await ftdi.FLASH_cs_deassert(device);
-//}
 
 //---------------------
 //-- UTILS
